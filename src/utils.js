83198868e6cd202603eed6fdaf68d3a44b21a5fb
Arashi-Crow <ignaciofdce92@gmail.com>
2024-12-26 15:52:16 -0600

    done

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,3 @@
 export const getImageUrl = (imageSrc) => {
-    const basePath = new URL('../assets/', import.meta.url).href;
-    console.log("basePath",`${basePath}/${imageSrc}`);
-    return `${basePath}/${imageSrc}`;
+    return `/assets/${imageSrc}`;
 };
